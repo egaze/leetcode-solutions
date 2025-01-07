0a17b0b1d783b444faee060d9b44a6d932ecb1cc
Eyiram Gaze <eyiramgaze@gmail.com>
2025-01-07 22:49:26 +0000

    Solution to The Index of the First Occurence in a String

---
+++ python/28_The_Index_of_the_First_Occurence_in_a_String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        first_occurence = haystack.find(needle)
+        
+        return first_occurence
+        
+
+        # May have to be solved again
